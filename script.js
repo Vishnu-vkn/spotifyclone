8d52330843fd0b4411b1f66b4bee84d1289a952a
Vishnu-vkn <vishnuvkn2004@gmail.com>
2025-12-17 23:57:47 +0530

    17/12/25

--- script.js
+++ script.js
@@ -99,6 +99,16 @@ async function main() {
         currentsong.currentTime=((currentsong.duration)*percent)/100;
     })
     
+    //Add an event listener for hamburger
+    document.querySelector(".hamburger").addEventListener("click",()=>{
+        document.querySelector(".left").style.left="0%"
+    })
+
+    //Add an event listener for close button
+    document.querySelector(".close").addEventListener("click",()=>{
+        document.querySelector(".left").style.left="-100%"
+    })
+
 
 }
 main()
